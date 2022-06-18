fa65719614b76ebded1cf4d39ddbfcc708985f6b
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2022-06-18 22:07:30 +0430

    inital styles

---
+++ antd.custom.js
@@ -0,0 +1,11 @@
+const modifyVars = {
+  "@primary-color": "#F9AFEC",
+
+  "@border-radius-base": "15px",
+  // Buttons 
+  "@btn-font-weight": 400,
+  
+  "@body-background": "#FFF5FD",
+};
+
+module.exports = modifyVars;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const withAntdLess = require('next-plugin-antd-less');
+const modifyVars = require('./antd.custom.js');
 
-module.exports = nextConfig
+module.exports = withAntdLess({
+  // optional
+  modifyVars: modifyVars,
+  // optional
+  lessVarsFilePath: './src/styles/variables.less',
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+
+  // ONLY for Next.js 10, if you use Next.js 11, delete this block
+  future: {
+    webpack5: true,
+  },
+});
