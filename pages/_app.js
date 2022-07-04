c7ca33cda46f126041c482117cc4ca3b73cd9989
Mohsen Poureiny <85901806+Mohsenpoureiny@users.noreply.github.com>
2022-07-04 23:21:26 +0430

    Add color to mobile browser head

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,19 @@ import { ConfigProvider } from 'antd';
 import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store, wrapper } from '../store/store';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+  }, []);
+
   return <>
+    <Head>
+      <meta name="theme-color" content="#F9AFEC" />
+      <meta name="msapplication-navbutton-color" content="#F9AFEC" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="#F9AFEC" />
+    </Head>
     <Provider store={store}>
       <ConfigProvider direction="rtl">
         <Component {...pageProps} />
