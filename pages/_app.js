50c208a205092a721a1306aedadf2b7e036a445e
Mohsen Poureiny <85901806+Mohsenpoureiny@users.noreply.github.com>
2022-07-06 12:45:17 +0430

    Add SEO meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -14,9 +14,27 @@ function MyApp({ Component, pageProps }) {
 
   return <>
     <Head>
+      {/* Seo Meta Tags */}
+      <meta name="title" content="فروشگاه اینترنتی روپومدا" />
+      <meta name="description" content="توضیحات" />
+      <meta name="keywords" content="لباس،تیشرت،مانتو" />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="revisit-after" content="7 days" />
+
+      {/* Theme Meta Tags */}
       <meta name="theme-color" content="#F9AFEC" />
       <meta name="msapplication-navbutton-color" content="#F9AFEC" />
       <meta name="apple-mobile-web-app-status-bar-style" content="#F9AFEC" />
+
+      {/* OG meta tags */}
+
+      <meta property="og:title" content="فروشگاه اینترنتی روپومدا" />
+      <meta property="og:site_name" content="Ropo Moda" />
+      <meta property="og:url" content="ropomoda.com" />
+      <meta property="og:description" content="فروشگاه اینترنتی روپومدا" />
+      <meta property="og:type" content="business.business" />
+      <meta property="og:image" content="https://ropomoda-public-staticfiles.s3.ir-thr-at1.arvanstorage.com/public/logo-fill.png" />
     </Head>
     <Provider store={store}>
       <ConfigProvider direction="rtl">
