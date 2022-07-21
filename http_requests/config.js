32f1e114c12b0d85a9aae615b4b11a9f2d4f7bcb
Mohsen Poureiny <85901806+Mohsenpoureiny@users.noreply.github.com>
2022-07-22 00:57:33 +0430

    Fix api base url issue

--- http_requests/config.js
+++ http_requests/config.js
@@ -1,5 +1,7 @@
-export const baseUrl = process.env["ROPOMODA_API_BASE_URL"] || '';
-
+export const BASE_URL =
+    process.env.NODE_ENV === "production"
+        ? process.env.NEXT_PUBLIC_API_BASE_URL
+        : "http://localhost:8525";
 
 export const customerConfig = () => {
     return {
--- http_requests/storeReq/product.req.js
+++ http_requests/storeReq/product.req.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { makeRequest, baseUrl, visitorConfig } from '../config';
+import { makeRequest, BASE_URL, visitorConfig } from '../config';
 
 
 
 export const getAllProducts = () => {
-    return makeRequest(() => axios.get(`${baseUrl}store/products/?format=json`, visitorConfig())
+    return makeRequest(() => axios.get(`${BASE_URL}store/products/?format=json`, visitorConfig())
     )
 }
