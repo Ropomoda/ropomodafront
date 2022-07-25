bea54d16a50781e4e75ed5ee9441e0dd80f7d825
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2022-07-26 04:24:23 +0430

    Remove format json

--- http_requests/storeReq/product.req.js
+++ http_requests/storeReq/product.req.js
@@ -4,6 +4,6 @@ import { makeRequest, BASE_URL, visitorConfig } from '../config';
 
 
 export const getAllProducts = () => {
-    return makeRequest(() => axios.get(`${BASE_URL}store/products/?format=json`, visitorConfig())
+    return makeRequest(() => axios.get(`${BASE_URL}store/products/`, visitorConfig())
     )
 }
